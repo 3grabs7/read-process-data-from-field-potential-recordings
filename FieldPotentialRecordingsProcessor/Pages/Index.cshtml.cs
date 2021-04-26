bfd8f5770a3d069665ea875605c3df1b706ea741
Robin Grabs <robinmgrabs@gmail.com>
2021-04-26 12:52:30 +0200

    yield return for csv parser. note: donät rely on db id, itterate for each csv to make sure placement is saved. Time prop

--- FieldPotentialRecordingsProcessor/Pages/Index.cshtml.cs
+++ FieldPotentialRecordingsProcessor/Pages/Index.cshtml.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FieldPotentialRecordingsProcessor.Data;
+using FieldPotentialRecordingsProcessor.Models;
+using FieldPotentialRecordingsProcessor.Utils;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,16 +16,49 @@ namespace FieldPotentialRecordingsProcessor.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<IndexModel> _logger;
+        private IWebHostEnvironment _environment;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+        [BindProperty]
+        public string Separator { get; set; }
+        [BindProperty]
+        public string LineSeparator { get; set; }
+        public RecordingSession UploadedRecordingSession { get; set; }
+
+        public IndexModel(ApplicationDbContext context,
+            ILogger<IndexModel> logger,
+            IWebHostEnvironment environment)
         {
+            _context = context;
             _logger = logger;
+            _environment = environment;
         }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task OnPostUploadRecordingSessionAsync()
+        {
+            var path = Path.Combine(_environment.ContentRootPath, "CSVData", $"{DateTime.Now.ToShortDateString()}.csv");
+            try
+            {
+                using (var fileStream = new FileStream(path, FileMode.Create))
+                {
+                    await Upload.CopyToAsync(fileStream);
+                }
+                var parser = new ParseCsv(path);
+                parser.Parse(_context);
+
+            }
+            catch
+            {
+
+            }
         }
     }
 }
---
+++ FieldPotentialRecordingsProcessor/Utils/ParseCsv.cs
@@ -0,0 +1,108 @@
+﻿using FieldPotentialRecordingsProcessor.Data;
+using FieldPotentialRecordingsProcessor.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace FieldPotentialRecordingsProcessor.Utils
+{
+    public class ParseCsv
+    {
+        private string FilePath { get; set; }
+        private string Separator { get; set; }
+        private string LineSeparator { get; set; }
+
+        public bool Success { get; set; } = true;
+
+        public ParseCsv(string filePath)
+        {
+            FilePath = filePath;
+            Separator = ";";
+        }
+
+        public ParseCsv(string filePath, string separator)
+        {
+            FilePath = filePath;
+            Separator = separator;
+        }
+
+        public ParseCsv(string filePath, string separator, string lineSeparator)
+        {
+            FilePath = filePath;
+            Separator = separator;
+            LineSeparator = lineSeparator;
+        }
+
+        public void Parse(ApplicationDbContext context)
+        {
+            var recordingSession = new RecordingSession()
+            {
+                Date = DateTime.Now,
+                Rat = null
+            };
+            context.Add(recordingSession);
+            context.SaveChanges();
+            try
+            {
+                var reflections = new RawData().GetType().GetProperties()
+                    .Where(p => !new string[] { "Id", "RecordingSession" }.Contains(p.Name))
+                    .ToArray();
+                var readCsv = File.ReadAllLines(FilePath);
+                for (int i = 1; i < readCsv.Length; i++)
+                {
+                    var row = readCsv[i].Split(Separator);
+                    var rawData = new RawData() { RecordingSession = recordingSession };
+                    for (int j = 0; j < reflections.Length; j++)
+                    {
+                        reflections[j].SetValue(rawData, Convert.ToDecimal(row[j]));
+                    }
+                    context.Add(rawData);
+                    context.SaveChanges();
+                }
+            }
+            finally
+            {
+                Success = true;
+            }
+
+        }
+    }
+}
+
+
+
+//public IEnumerable<RawData> Parse(ApplicationDbContext context)
+//{
+//    var recordingSession = new RecordingSession()
+//    {
+//        Date = DateTime.Now,
+//        Rat = null
+//    };
+//    context.Add(recordingSession);
+//    context.SaveChanges();
+//    try
+//    {
+//        var reflections = new RawData().GetType().GetProperties()
+//            .Where(p => !new string[] { "Id", "RecordingSession" }.Contains(p.Name))
+//            .ToArray();
+//        var readCsv = File.ReadAllLines(FilePath);
+//        for (int i = 1; i < readCsv.Length; i++)
+//        {
+//            var row = readCsv[i].Split(Separator);
+//            var rawData = new RawData() { RecordingSession = recordingSession };
+//            for (int j = 0; j < reflections.Length; j++)
+//            {
+//                reflections[j].SetValue(rawData, Convert.ToDecimal(row[j]));
+//            }
+//            yield return rawData;
+//        }
+//    }
+//    finally
+//    {
+//        Success = true;
+//    }
+
+//}
